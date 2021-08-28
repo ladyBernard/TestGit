fcaf78fd8f2838094bd424fce2cbad6df0903a45
伯纳德小姐 <2018065899@qq.com>
2021-08-28 10:10:21 +0800

    first commit

---
+++ src/com/lady/test/Test.java
@@ -0,0 +1,11 @@
+package com.lady.test;
+
+/**
+ * @author ladybernard
+ * @date 2021/8/28 10:08:18
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("lady写的代码");
+    }
+}
